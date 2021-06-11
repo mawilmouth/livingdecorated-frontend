91faee64af115a96f9c840d9b23e76896b48325c
Michael Wilmouth <wilmouthworks@gmail.com>
2021-06-10 21:55:55 -0400

    Get foundation installed and imported

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/application.scss'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import '../styles/application.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
