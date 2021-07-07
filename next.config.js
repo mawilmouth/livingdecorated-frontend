6ce3f10f9ed593642b7208f1482af96a22dd08bf
Michael Wilmouth <wilmouthworks@gmail.com>
2021-07-06 20:38:14 -0400

    Upgrade React and Next versions and use new Image component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const withImages = require('next-images');
 
-module.exports = withImages({
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -15,4 +14,4 @@ module.exports = withImages({
   webpack(config, options) {
     return config;
   }
-});
+};
