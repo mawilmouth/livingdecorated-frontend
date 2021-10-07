2395857ed8c7b562e14d03398184c4299c275d6e
Michael Wilmouth <wilmouthworks@gmail.com>
2021-10-06 22:33:55 -0400

    Create helper method to render the feature image of a post or page

---
+++ helpers/renderFeatureImage.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import { PageType } from "../types/lib/ghost/pages";
+import { PostType } from "../types/lib/ghost/posts";
+
+
+export default function renderFeatureImage (data: PageType | PostType): ReactElement | null {
+  const { title, feature_image: featureImage } = data;
+
+  return featureImage ? <img src={featureImage} alt={title} /> : null;
+}
