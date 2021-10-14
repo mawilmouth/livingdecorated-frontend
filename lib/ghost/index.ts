a13e5a953406466a9da0c74b29a22580dcfe6f89
Michael Wilmouth <wilmouthworks@gmail.com>
2021-10-14 15:19:17 -0400

    Remove unwanted console log call

--- lib/ghost/index.ts
+++ lib/ghost/index.ts
@@ -2,8 +2,6 @@ import GhostApi from '@tryghost/content-api';
 import env from '../../constants/env';
 import { GhostApiType } from '../../types/lib/ghost';
 
-console.log('here', env);
-
 const ghost: GhostApiType = new GhostApi({
   url: env.ghostUrl,
   key: env.ghostContentKey,
